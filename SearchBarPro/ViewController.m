8ac4b70831193b5c5e6ddb334c8c0bac8f8bde9e
lyroger <jp.8586965>
2015-12-02 17:59:34 +0800

    优化页面
    仿照微信首页搜索效果

--- SearchBarPro/ViewController.m
+++ SearchBarPro/ViewController.m
@@ -12,6 +12,13 @@
 #define mScreenHeight           ([UIScreen mainScreen].bounds.size.height)
 #define mRGBColor(r, g, b)      [UIColor colorWithRed:r/255.0 green:g/255.0 blue:b/255.0 alpha:1.0]
 #define GreyishWhiteColor       mRGBColor(242, 242, 242)
+#define GrayWhiteColor          mRGBColor(230, 230, 230)
+
+// 定义这个常量,就可以在使用Masonry不必总带着前缀 `mas_`:
+#define MAS_SHORTHAND
+// 定义这个常量,以支持在 Masonry 语法中自动将基本类型转换为 object 类型:
+#define MAS_SHORTHAND_GLOBALS
+#import "Masonry.h"
 
 @interface ViewController ()<UISearchDisplayDelegate,UISearchBarDelegate,UITableViewDataSource,UITableViewDelegate>
 {
@@ -21,6 +28,11 @@
     
     NSArray *dataArray;
     NSArray *searchDataArray;
+    UIButton *btnVoice;
+    UIButton *btnCancel;
+    UIView  *searchBgView;
+    CALayer *topLineLayer;
+    CALayer *bottomLayer;
 }
 @property (nonatomic,strong) UIView *tempSearchDisplayBackgroungView;
 @end
@@ -36,8 +48,8 @@
 
 - (void)loadSubView
 {
-//    dataArray = @[@"第一行",@"第二行",@"第三行",@"第四行",@"第五行"];
-    dataArray = @[];
+    dataArray = @[@"第一行",@"第二行",@"第三行",@"第四行",@"第五行"];
+    self.view.backgroundColor = GreyishWhiteColor;
     searchDataArray = @[@"搜索第一行"];
     
     if (!self.tempSearchDisplayBackgroungView) {
@@ -62,20 +74,48 @@
         
     }
     
-    mySearchBar = [[UISearchBar alloc] initWithFrame:CGRectMake(0, 0, mScreenWidth, 40)];
+    btnVoice = [UIButton buttonWithType:UIButtonTypeCustom];
+    [btnVoice setImage:[UIImage imageNamed:@"icon_voice"] forState:UIControlStateNormal];
+    [btnVoice addTarget:self action:@selector(btnVoiceClick) forControlEvents:UIControlEventTouchUpInside];
+    
+    btnCancel = [UIButton buttonWithType:UIButtonTypeCustom];
+    [btnCancel setTitleColor:mRGBColor(78, 216, 101) forState:UIControlStateNormal];
+    btnCancel.titleLabel.font = [UIFont systemFontOfSize:16];
+    [btnCancel setTitle:@"取消" forState:UIControlStateNormal];
+    
+    
+    mySearchBar = [[UISearchBar alloc] initWithFrame:CGRectMake(0, 0, mScreenWidth, 44)];
     mySearchBar.delegate = self;
-    mySearchBar.barTintColor = [UIColor whiteColor];
     [mySearchBar setPlaceholder:@"搜索"];
-    UIColor *bgColor = [UIColor colorWithRed:0xff green:0xff blue:0xff alpha:1];
-    mySearchBar.backgroundImage = [self imageFromColor:bgColor frame:mySearchBar.bounds];
+    mySearchBar.backgroundColor = mRGBColor(239, 239, 244);
+    mySearchBar.backgroundImage = [self imageFromColor:mRGBColor(239, 239, 244) frame:mySearchBar.bounds];
     for (UIView *subView in mySearchBar.subviews)
     {
         for (UIView *secondLevelSubview in subView.subviews){
             if ([secondLevelSubview isKindOfClass:[UITextField class]])
             {
                 UITextField *searchBarTextField = (UITextField *)secondLevelSubview;
-//                searchBarTextField.textColor = mRGBColor(155, 155, 155);
-                searchBarTextField.backgroundColor = GreyishWhiteColor;
+                searchBarTextField.backgroundColor = [UIColor whiteColor];
+                searchBarTextField.layer.borderColor =  mRGBColor(228, 229, 233).CGColor;
+                searchBarTextField.layer.borderWidth = 1;
+                searchBarTextField.layer.cornerRadius = 4;
+                searchBarTextField.tintColor = mRGBColor(78, 216, 101);
+                
+                CGFloat btnWidth = 25;
+                [searchBarTextField addSubview:btnVoice];
+                [searchBarTextField.superview addSubview:btnCancel];
+                
+                [btnVoice makeConstraints:^(MASConstraintMaker *make) {
+                    make.right.equalTo(-10);
+                    make.centerY.equalTo(searchBarTextField.centerY);
+                    make.size.equalTo(CGSizeMake(btnWidth, btnWidth));
+                }];
+                
+                [btnCancel makeConstraints:^(MASConstraintMaker *make) {
+                    make.left.equalTo(searchBarTextField.right).offset(-2);
+                    make.centerY.equalTo(searchBarTextField.centerY);
+                    make.size.equalTo(CGSizeMake(50, 30));
+                }];
                 break;
             }
         }
@@ -101,6 +141,34 @@
     [self.view addSubview:tableViewList];
 }
 
+- (void)searchBarTextDidBeginEditing:(UISearchBar*)searchBar
+{
+    [searchBar setShowsCancelButton:YES animated:YES];
+    
+    UIButton *btn = [searchBar valueForKey:@"_cancelButton"];
+    [btn setTitle:@"      " forState:UIControlStateNormal];
+}
+
+- (void)searchBar:(UISearchBar *)searchBar textDidChange:(NSString *)searchText
+{
+    if (searchText.length) {
+        btnVoice.hidden = YES;
+    } else {
+        btnVoice.hidden = NO;
+    }
+}
+
+- (void)btnVoiceClick
+{
+    NSLog(@"btnVoiceClick");
+}
+
+- (void)searchDisplayControllerWillEndSearch:(UISearchDisplayController *)controller
+{
+    topLineLayer.hidden = YES;
+    bottomLayer.hidden = YES;
+}
+
 - (void)searchDisplayControllerWillBeginSearch:(UISearchDisplayController *)controller
 {
     [self performSelector:@selector(addTipsViewWithView:) withObject:controller afterDelay:0.1];
@@ -108,14 +176,13 @@
 
 - (void)addTipsViewWithView:(UISearchDisplayController*)controller
 {
-    NSLog(@"subViews.count = %zd",controller.searchResultsTableView.superview.subviews.count);
-    NSLog(@"superView %@",[controller.searchResultsTableView.superview class]);
     UIView *supV = controller.searchResultsTableView.superview;
     UIView *supsupV = supV.superview;
     
     for (UIView *view in supsupV.subviews) {
         for (UIView *sencondView in view.subviews) {
-            if ([sencondView isKindOfClass:[NSClassFromString(@"_UISearchDisplayControllerDimmingView") class]]) {
+            if ([sencondView isKindOfClass:[NSClassFromString(@"_UISearchDisplayControllerDimmingView") class]])
+            {
                 NSLog(@"_UISearchDisplayControllerDimmingView");
                 if (![sencondView viewWithTag:99]) {
                     [sencondView addSubview:self.tempSearchDisplayBackgroungView];
@@ -124,6 +191,50 @@
             }
         }
     }
+    
+    if (!searchBgView) {
+        for (UIView *subView in mySearchBar.subviews)
+        {
+            for (UIView *secondLevelSubview in subView.subviews){
+                if ([secondLevelSubview isKindOfClass:[NSClassFromString(@"UISearchBarBackground") class]])
+                {
+                    [self addBgViewToView:secondLevelSubview];
+                    break;
+                }
+            }
+        }
+    }
+}
+
+- (void)addBgViewToView:(UIView*)view
+{
+    if (!searchBgView) {
+        searchBgView = [[UIView alloc] init];
+        searchBgView.tag = 100;
+        searchBgView.backgroundColor = mRGBColor(239, 239, 244);
+        [view addSubview:searchBgView];
+        
+        [searchBgView makeConstraints:^(MASConstraintMaker *make) {
+            make.size.equalTo(view.frame.size);
+            make.left.equalTo(view.left);
+            make.right.equalTo(view.right);
+            make.top.equalTo(view.top);
+            make.bottom.equalTo(view.bottom);
+        }];
+        
+        topLineLayer = [CALayer layer];
+        topLineLayer.frame = CGRectMake(0, 20, mScreenWidth, 1);
+        topLineLayer.backgroundColor = GrayWhiteColor.CGColor;
+        [searchBgView.layer addSublayer:topLineLayer];
+        
+        bottomLayer = [CALayer layer];
+        bottomLayer.frame = CGRectMake(0, 63, mScreenWidth, 1);
+        bottomLayer.backgroundColor = GrayWhiteColor.CGColor;
+        [searchBgView.layer addSublayer:bottomLayer];
+    }
+    
+    topLineLayer.hidden = NO;
+    bottomLayer.hidden = NO;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
